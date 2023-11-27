e4e78a3380a0aa8ab1b95981e639ef75a7c81e75
OmBharsakle <149371245+OmBharsakle@users.noreply.github.com>
2023-11-27 22:46:51 +0530

    Add files via upload

---
+++ 10. Looping With Pattern/01.c
@@ -0,0 +1,16 @@
+//1. Write C Program To Print All Alphabets From a to z. Using do While loop.
+#include<stdio.h>
+main()
+{
+	char n = 'a';
+	
+	do
+	{
+		printf("%c ",n);
+		n++;
+	}while(n<='z');
+}
+
+
+
+
