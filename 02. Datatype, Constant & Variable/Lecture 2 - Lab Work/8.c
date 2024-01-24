7d247d8bb0ab95456c80cd8aef77ed6465b74723
OmBharsakle <149371245+OmBharsakle@users.noreply.github.com>
2024-01-24 10:27:06 +0530

    Update 8.c

--- 02. Datatype, Constant & Variable/Lecture 2 - Lab Work/8.c
+++ 02. Datatype, Constant & Variable/Lecture 2 - Lab Work/8.c
@@ -1,21 +1,23 @@
-
 #include<stdio.h>
-main()
-{
-	int a,b;
-	//Input A/B value
-	printf("Enter The Value Of A : ");
-	scanf("%d",&a);//5
-	printf("Enter The Value Of B : ");
-	scanf("%d",&b);//10
-	
-	//Logic 
-	a=a-b;//-5
-	b=a+b;//-5+10=5
-	a=b-a;//10
-	
-	//print Value
-	printf("A Value Of : %d\n",a);
-	printf("B Value Of : %d",b);
-	
+
+int main() {
+    int a, b;
+
+    // Input A/B value
+    printf("Enter The Value Of A : ");
+    scanf("%d", &a); // Input: 5
+    
+    printf("Enter The Value Of B : ");
+    scanf("%d", &b); // Input: 10
+
+    // Swap the values of a and b without using a third variable
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    // Print Values
+    printf("A Value Of : %d\n", a); // Output: 10
+    printf("B Value Of : %d", b);   // Output: 5
+
+    return 0;
 }
